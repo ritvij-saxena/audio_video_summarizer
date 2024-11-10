4992bac2315ffe54cf4566c4b348117f88540360
ritvij-saxena <saxenaritvij@gmail.com>
2024-11-10 00:33:12 -0500

    txed tp

--- transcriber.py
+++ transcriber.py
@@ -4,6 +4,8 @@ import ffmpeg
 import os
 import subprocess
 
+os.environ['FFMPEG_BINARY'] = os.path.join("third-party", "ffmpeg", "ffmpeg")
+
 def transcribe_audio(audio_path):
     """Transcribe audio to text using whisper.cpp."""
     logging.info(f"Transcribing audio file: {audio_path}")
@@ -13,6 +15,7 @@ def transcribe_audio(audio_path):
         logging.error("Invalid file format: Only .wav files are supported.")
         return None
 
+    logging.info(f"Validating Sample Rate...")
     # Validate the sample rate is 16 kHz
     if not is_16khz_wav(audio_path):
         try:
@@ -31,22 +34,27 @@ def transcribe_audio(audio_path):
 
 def is_16khz_wav(audio_path):
     """Check if a .wav file has a 16 kHz sampling rate."""
+    logging.info(f"Checking if a .wav file has a 16 kHz sampling rate.")
     try:
         with wave.open(audio_path, 'rb') as wav_file:
             sample_rate = wav_file.getframerate()
+            logging.info(f"Current Sample Rate: {sample_rate}")
             return sample_rate == 16000
     except wave.Error as e:
         logging.error(f"Error reading .wav file: {e}")
+        logging.error(f"Error: {e}")
         return False
 
 def convert_to_16khz(audio_path):
     """Convert an audio file to 16 kHz using ffmpeg-python."""
+    logging.info("Converting the audio file to 16 kHz using ffmpeg-python.")
     copy_audio_path = audio_path
+    logging.info("Copying and renaming audio path...")
     converted_path = copy_audio_path.replace(".wav", "_16khz.wav")
 
     try:
+        logging.info(f"Converting audio to 16 kHz: {converted_path}")
         ffmpeg.input(audio_path).output(converted_path, ar=16000).run(overwrite_output=True)
-        logging.info(f"Converted audio to 16 kHz: {converted_path}")
         return converted_path
     except ffmpeg.Error as e:
         logging.error(f"ffmpeg-python conversion failed: {e.stderr.decode('utf8')}")
