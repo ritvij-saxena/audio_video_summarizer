b1c4ddf1c6dabbe4e73556d9358a08c78b134c2d
ritvij-saxena <saxenaritvij@gmail.com>
2024-11-12 23:48:28 -0500

    youtube video test

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,4 +1,4 @@
-import youtube_dl
+import yt_dlp as youtube_dl
 import os
 import logging
 
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Default download path
 DEFAULT_DOWNLOAD_PATH = './youtube_downloads'
+FFMPEG_PATH = "third-party/ffmpeg/ffmpeg"
 
 def download_video_from_youtube(youtube_url: str):
     try:
@@ -16,6 +17,7 @@ def download_video_from_youtube(youtube_url: str):
 
         # Set up youtube-dl options
         ydl_opts = {
+            'ffmpeg_location': FFMPEG_PATH,
             'format': 'bestvideo+bestaudio/best',  # Download best video and audio
             'outtmpl': os.path.join(DEFAULT_DOWNLOAD_PATH, '%(title)s.%(ext)s'),  # Save video with title as filename
             'noplaylist': True,  # Prevent downloading playlists
@@ -23,6 +25,7 @@ def download_video_from_youtube(youtube_url: str):
         }
 
         # Download video using youtube-dl
+        logging.info("Downloading video using yt-dl")
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             result = ydl.extract_info(youtube_url, download=True)
             if 'entries' in result:
@@ -31,6 +34,7 @@ def download_video_from_youtube(youtube_url: str):
                 video_info = result
 
             # Return the path to the downloaded video file
+            logging.info("Returning the path to the downloaded video file")
             downloaded_video_path = os.path.join(DEFAULT_DOWNLOAD_PATH, video_info['title'] + '.' + video_info['ext'])
             logging.info(f"Downloaded video to: {downloaded_video_path}")
             return downloaded_video_path
