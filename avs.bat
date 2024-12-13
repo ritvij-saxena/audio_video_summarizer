ef00802422364f18b5074d570e38d9e667d5f5df
ritvij-saxena <saxenaritvij@gmail.com>
2024-12-12 23:46:18 -0500

    choose python available

--- avs.bat
+++ avs.bat
@@ -1,3 +1,21 @@
 @echo off
+setlocal
+
+:: Check for python3
+where python3 >nul 2>&1
+if %errorlevel%==0 (
+    set PYTHON_CMD=python3
+) else (
+    :: Fallback to python
+    where python >nul 2>&1
+    if %errorlevel%==0 (
+        set PYTHON_CMD=python
+    ) else (
+        echo Python is not installed or not added to PATH.
+        exit /b 1
+    )
+)
+
+:: Run the Python script
 set DIR=%~dp0
-python "%DIR%avs" %*
+%PYTHON_CMD% "%DIR%avs" %*
